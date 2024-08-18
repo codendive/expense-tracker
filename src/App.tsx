0dcce6add192c9e366f3eac1400fd392417e019f
codendive <codendive@gmail.com>
2024-08-18 15:23:55 +0200

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { useState } from 'react'
+import ExpenseForm from './components/ExpenseForm'
+import ExpenseFilter from './components/ExpenseFilter'
+import ExpenseList from './components/ExpenseList'
+import expensesData from './expenses'
+
 const App = () => {
-  return <div>App</div>
+  const [expenses, setExpenses] = useState(expensesData)
+  const [selectedCategory, setSelectedCategory] = useState('')
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses
+
+  return (
+    <div>
+      <div className='mb-3'>
+        <div className='mb-5'>
+          <ExpenseForm
+            onSubmit={(expense) =>
+              setExpenses([
+                ...expenses,
+                { ...expense, id: expenses.length + 1 }
+              ])
+            }
+          />
+        </div>
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
+    </div>
+  )
 }
 
 export default App
---
+++ src/expenses.ts
@@ -0,0 +1,34 @@
+const expensesData = [
+  { id: 1, description: 'Grocery Shopping', amount: 50, category: 'Groceries' },
+  { id: 2, description: 'Electricity Bill', amount: 75, category: 'Utilities' },
+  {
+    id: 3,
+    description: 'Movie Tickets',
+    amount: 30,
+    category: 'Entertainment'
+  },
+  { id: 4, description: 'Internet Bill', amount: 60, category: 'Utilities' },
+  { id: 5, description: 'Dinner Out', amount: 45, category: 'Entertainment' },
+  {
+    id: 6,
+    description: 'Weekly Groceries',
+    amount: 100,
+    category: 'Groceries'
+  },
+  { id: 7, description: 'Water Bill', amount: 25, category: 'Utilities' },
+  {
+    id: 8,
+    description: 'Concert Tickets',
+    amount: 120,
+    category: 'Entertainment'
+  },
+  {
+    id: 9,
+    description: 'Organic Groceries',
+    amount: 80,
+    category: 'Groceries'
+  },
+  { id: 10, description: 'Gas Bill', amount: 40, category: 'Utilities' }
+]
+
+export default expensesData
